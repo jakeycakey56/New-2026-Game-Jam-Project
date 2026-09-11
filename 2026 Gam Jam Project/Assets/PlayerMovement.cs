e71ef8cabc916ce42994879a67ae6d6a5cc46ea9
Jake Preston <70823924+jakeycakey56@users.noreply.github.com>
2026-09-11 12:18:05 -0700

    Player Move

--- 2026 Gam Jam Project/Assets/PlayerMovement.cs
+++ 2026 Gam Jam Project/Assets/PlayerMovement.cs
@@ -4,17 +4,41 @@ using UnityEngine.AI;
 public class PlayerMovement : MonoBehaviour
 {
     private NavMeshAgent PlayerAgent;
+    public Camera PlayerCamera;
+
+    public float MoveSpeed = 10f;
+
+    [SerializeField] float SampleDistance = .5f;
+    [SerializeField] LayerMask GroundLayer;
+
+    public static event System.Action<Vector3> OnGroundTouch;
 
     void Start()
     {
         PlayerAgent = GetComponent<NavMeshAgent>();
+
+
     }
 
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
+            Ray ray = PlayerCamera.ScreenPointToRay(Input.mousePosition);
 
+            if (Physics.Raycast(ray, out RaycastHit hit, GroundLayer))
+            {
+                if (NavMesh.SamplePosition(hit.point, out NavMeshHit navMeshHit, SampleDistance, NavMesh.AllAreas))
+                {
+                    PlayerAgent.SetDestination(navMeshHit.position);
+
+                    OnGroundTouch?.Invoke(navMeshHit.position);
+                }
+                else
+                {
+                    Debug.Log("You clicked outside of the walkable Area");
+                }
+            }
         }
     }
 }
